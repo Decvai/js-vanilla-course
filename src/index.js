0a3ab9d7a410bff61f3d7ae1fdd6b742e3bce3db
Cvai <vladyslav.katrych.main@gmail.com>
2021-04-27 17:33:13 +0300

    redux init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Formula } from '@/components/formula/Formula';
 import { Header } from '@/components/headers/Header';
 import { Table } from '@/components/table/Table';
 import { Toolbar } from '@/components/toolbar/Toolbar';
+import { createStore } from '@core/createStore';
+
+const store = createStore();
 
 const excel = new Excel('#app', {
 	components: [Header, Toolbar, Formula, Table],
